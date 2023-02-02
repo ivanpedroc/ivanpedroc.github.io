474101623e0684d96caf89608be9567198554e36
ivanpedroc <122410036+ivanpedroc@users.noreply.github.com>
2023-02-02 08:38:21 -0300

    Aula 14
    Meu site

--- myHeader.js
+++ myHeader.js
@@ -1,42 +1,50 @@
+const myLinks = [
+    ["Inicio", "/"],
+    ["Teste", "/teste.html"],
+    ["Quem sou", "/quem sou.html"],
+    ["Projeto 1", "/projects/projeto1.html"],
+    ["Curriculo", "/curriculo.html"],
+    ["GitHub","https://github.com/ivanpedroc"],
+    ["Linkedin","https://www.linkedin.com/in/ivan-santos-8aa19a220"],
+
+];
+
 class MyHeader extends HTMLElement{
     connectedCallback() {
         //createElement = cria uma elemento HTML baseado numa TAG especificada
         const header = document.createElement("header");
-        const nav = document.createElement("nav");
-        const ul = document.createElement("ul");
         const titleDiv = document.createElement("div");
         const pageTitle = document.createElement("h1");
 
-
-        const myLinks = [
-            ["Inicio", "/"],
-            ["Teste", "/teste.html"],
-            ["Projeto 1", "/projects/projeto1.html"]
-
-
-        ];
-
-
-        for(let i = 0; i < myLinks.length; i++){
-            let li = document.createElement("li");
-            let a = document.createElement("a");
-            a.setAttribute("href", myLinks[i][1]);
-            a.textContent = myLinks[i][0];
-            li.appendChild(a);
-            ul.appendChild(li);
-        }
-
-
-        nav.appendChild(ul);
         pageTitle.textContent = "Meu Site";
         titleDiv.appendChild(pageTitle);
         header.appendChild(titleDiv);
-        header.appendChild(nav);
+        header.appendChild(navBar(myLinks));
         this.appendChild(header);
-
 
     }
 
-
 }
-customElements.define("my-header", MyHeader);
+
+function navBar(webpageList) {
+    //Essa função pega uma lista de sites, e gera uma barra de navegação
+    const nav = document.createElement("nav");
+    const ul = document.createElement("ul");
+
+    //Esse loop cria um item de lista com um link para cada um dos elementos do webpageList
+    for(let i = 0; i < webpageList.length; i++){
+        let li = document.createElement("li");
+        let a = document.createElement("a");
+        a.setAttribute("href", webpageList[i][1]);
+        a.textContent = webpageList[i][0];
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+
+    //adiciono minha lista não ordenada ao meu nav, e retorno meu nav
+    nav.appendChild(ul);
+    return nav;
+}
+
+customElements.define("my-header", MyHeader);d
+
